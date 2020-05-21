065021555ebd1ddd9a1b93203f055255be09b382
soum-sr <soum1071@gmail.com>
2020-05-22 01:03:13 +0530

    updated code

--- immigration.py
+++ immigration.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import numpy as np 
 import pandas as pd 
+import matplotlib.pyplot as plt
 
 st.title('Immigration to Canada')
 df_ca = pd.read_excel('Canada.xlsx',sheet_name='Canada by Citizenship',skiprows=range(20), skipfooter=2)
@@ -28,7 +29,17 @@ years = list(map(str, range(1980,2014)))
 
 # Plotting Immigration from India
 india_df = df_ca.loc['India', years]
-india_df.index = india_df.index.map(int)
+# india_df.index = india_df.index.map(int)
 
 st.write("""### Immigration from India""")
 st.line_chart(india_df)
+
+# Top 5 countries
+
+df_ca.sort_values(by='Total', ascending=False, axis=0, inplace=True)
+df_top5 = df_ca.head(5)
+df_top5 = df_top5[years].transpose()
+data_top5 = pd.DataFrame(np.array(df_top5), columns= df_top5.columns.values)
+
+st.write("""### Top 5 Countries with Highest""")
+st.line_chart(data_top5)
